eede244525e95e7bd5676a8cf501a6e846d04056
ry26 <riku75pancras'gmail.com>
2018-04-05 18:18:58 +0100

    initial

--- src/SearchAPI.java
+++ src/SearchAPI.java
@@ -1,6 +1,121 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class SearchAPI {
-    public void startFunctions(String searchCategory, String queryString, String cacheDirectoryPath){
-        
+
+    public void startFunctions(String searchCategory, String queryString, String cacheDirectoryPath) throws Exception {
+        searchCategory = convertCategoryIntoURL(searchCategory);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("http://dblp.org/search/");
+        stringBuilder.append(searchCategory);
+        stringBuilder.append("/api?q=");
+        stringBuilder.append(URLEncoder.encode(queryString, "UTF-8"));
+        stringBuilder.append("&format=xml");
+        stringBuilder.append("&h=40");
+        stringBuilder.append("&c=0");
+
+        String urlString = stringBuilder.toString();
+        URL url = new URL(urlString);
+
+        InputStream inputStream = url.openStream();
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(inputStream);
+        doc.getDocumentElement().normalize();
+
+        if (searchCategory.equals("publ")) {
+            printPublicationInfo(doc);
+        } else if (searchCategory.equals("author")) {
+            printAuthorInfo(doc);
+        } else {
+            printVenueInfo(doc);
+        }
+
+
+    }
+
+    private String convertCategoryIntoURL(String searchCategory) {
+        if (searchCategory.equals("publication")) {
+            searchCategory = "publ";
+        }
+
+        return searchCategory;
+    }
+
+    private void printPublicationInfo(Document doc) throws XPathExpressionException {
+        NodeList publications = doc.getElementsByTagName("info");
+
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xp = xpf.newXPath();
+        XPathExpression xpeTitle = xp.compile("title");
+        XPathExpression xpeAuthor = xp.compile("authors/author");
+
+        for (int i = 0; i < publications.getLength(); i++){
+            String title = (String) xpeTitle.evaluate(publications.item(i), XPathConstants.STRING);
+            NodeList authorList = (NodeList) xpeAuthor.evaluate(publications.item(i), XPathConstants.NODESET);
+
+
+            System.out.println(title + " (number of authors: " + authorList.getLength() + ")");
+        }
+    }
+
+    private void printAuthorInfo(Document doc) throws XPathExpressionException, IOException, ParserConfigurationException, SAXException {
+        NodeList authors = doc.getElementsByTagName("info");
+
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xp = xpf.newXPath();
+        XPathExpression xpeURL = xp.compile("url");
+        XPathExpression xpeAuthor = xp.compile("author");
+
+        for (int i = 0; i < authors.getLength(); i++) {
+            String title = (String) xpeAuthor.evaluate(authors.item(i), XPathConstants.STRING);
+            System.out.print(title + " - ");
+
+            String url2 = (String) xpeURL.evaluate(authors.item(i), XPathConstants.STRING);
+            URL url = new URL(url2 + ".xml");
+
+            //TODO: make this a method
+            InputStream inputStream = url.openStream();
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc2 = db.parse(inputStream);
+            doc2.getDocumentElement().normalize();
+
+            NodeList publicationsList = doc2.getElementsByTagName("r");
+
+            System.out.print(publicationsList.getLength() + " publications with ");
+
+            NodeList coauthorsList = doc2.getElementsByTagName("co");
+            System.out.println(coauthorsList.getLength() + " publications");
+
+        }
+
+    }
+
+    private void printVenueInfo(Document doc) {
+        NodeList venues = doc.getElementsByTagName("venue");
+
+        for (int i = 0; i < venues.getLength(); i++) {
+            Element e = (Element) venues.item(i);
+            System.out.println(e.getTextContent());
+        }
+
 
     }
 }
--- src/W09Practical.java
+++ src/W09Practical.java
@@ -1,3 +1,4 @@
+import javax.xml.xpath.XPathExpressionException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,34 +9,36 @@ public class W09Practical {
 
 
     public static void main(String[] args) {
-        if(args.length != 6){
-            System.out.println("The length of the command line argument needs to be 6");
-        }
-        else if (!queryArray.contains(args[0]) && !queryArray.contains(args[2]) && !queryArray.contains(args[4])){
-            System.out.println("The search queries are invalid");
-        }
-        else {
-            String searchCategory = null;
-            String queryString = null;
-            String cacheDirectoryPath = null;
+        try {
+            if (args.length != 6) {
+                System.out.println("The length of the command line argument needs to be 6");
+            } else if (!queryArray.contains(args[0]) && !queryArray.contains(args[2]) && !queryArray.contains(args[4])) {
+                System.out.println("The search queries are invalid");
+            } else {
+                String searchCategory = null;
+                String queryString = null;
+                String cacheDirectoryPath = null;
 
-            for (int i = 0; i < args.length; i += 2) {
-                if (args[i].equals("--search")) {
-                    if (!args[i + 1].equals("author") && !args[i + 1].equals("publication") && !args[i + 1].equals("venue")) {
-                        System.out.println("The search category is invalid");
-                    } else {
-                        searchCategory = args[i + 1];
+                for (int i = 0; i < args.length; i += 2) {
+                    if (args[i].equals("--search")) {
+                        if (!args[i + 1].equals("author") && !args[i + 1].equals("publication") && !args[i + 1].equals("venue")) {
+                            System.out.println("The search category is invalid");
+                        } else {
+                            searchCategory = args[i + 1];
+                        }
+                    } else if (args[i].equals("--query")) {
+                        queryString = args[i + 1];
+                    } else if (args[i].equals("--cache")) {
+                        cacheDirectoryPath = args[i + 1];
                     }
-                } else if (args[i].equals("--query")) {
-                    queryString = args[i + 1];
-                } else if (args[i].equals("--cache")) {
-                    cacheDirectoryPath = args[i + 1];
+                }
+                if (searchCategory != null) {
+                    SearchAPI searchAPI = new SearchAPI();
+                    searchAPI.startFunctions(searchCategory, queryString, cacheDirectoryPath);
                 }
             }
-            if (searchCategory != null) {
-                SearchAPI searchAPI = new SearchAPI();
-                searchAPI.startFunctions(searchCategory, queryString, cacheDirectoryPath);
-            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
